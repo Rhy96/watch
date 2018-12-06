f338b92aea89c1218ff04c5c9ac1e2a92ac8b99e
Rhy96 <44130711+Rhy96@users.noreply.github.com>
2018-12-06 11:20:59 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+///Jangan hapus tanpa kutip nya(")
+///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
+///Ganti xxxxxxx dengan punya kalian
+///Capture dulu apk nya ambil yang di butuhkan
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBh3uVuuB5arOTi828iphsfayy24-5hWLAdX6akY273X53kNG64IqktLiHyq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$meid = "a48ab1956836f6ac";
+$os="android";
+$sign="7b2a5b4646e45ddd4b62363367856c169463e88cb0cd7994bdaf9907c0b42c20";
